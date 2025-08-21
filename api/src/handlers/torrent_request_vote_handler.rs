5d3c9132d041ef9daecdb2a51f41abc798cca9ce
ppoliani <ppoliani@gmail.com>
2025-08-21 14:24:29 +0300

    fix: torrent request vote handler

--- api/src/handlers/torrent_request_vote_handler.rs
+++ api/src/handlers/torrent_request_vote_handler.rs
@@ -1,12 +1,10 @@
-use crate::{
-    Arcadia, Result,
-    models::{
-        torrent_request_vote::{TorrentRequestVote, UserCreatedTorrentRequestVote},
-        user::User,
-    },
+use crate::{handlers::User, Arcadia};
+use actix_web::{HttpResponse, web};
+use arcadia_common::error::Result;
+use arcadia_storage::{
+    models::torrent_request_vote::{TorrentRequestVote, UserCreatedTorrentRequestVote},
     repositories::torrent_request_vote_repository::create_torrent_request_vote,
 };
-use actix_web::{HttpResponse, web};
 
 #[utoipa::path(
     post,
@@ -20,7 +18,7 @@ pub async fn add_torrent_request_vote(
     arc: web::Data<Arcadia>,
     current_user: User,
 ) -> Result<HttpResponse> {
-    let vote = create_torrent_request_vote(&arc.pool, &torrent_request_vote, &current_user).await?;
+    let vote = create_torrent_request_vote(arc.pool.borrow(), &torrent_request_vote, &current_user).await?;
 
     Ok(HttpResponse::Created().json(vote))
 }
