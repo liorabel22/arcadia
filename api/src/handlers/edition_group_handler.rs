b5aebea741c3fb482c1d2249d3cb6367fb63197f
ppoliani <ppoliani@gmail.com>
2025-08-21 14:00:06 +0300

    fix: edition group handler

--- api/src/handlers/edition_group_handler.rs
+++ api/src/handlers/edition_group_handler.rs
@@ -1,10 +1,12 @@
 use crate::{
-    Arcadia, Result,
-    handlers::UserId,
-    models::edition_group::{EditionGroup, UserCreatedEditionGroup},
-    repositories::edition_group_repository::create_edition_group,
+    Arcadia, handlers::UserId,
 };
 use actix_web::{HttpResponse, web};
+use arcadia_common::error::Result;
+use arcadia_storage::{
+  models::edition_group::{EditionGroup, UserCreatedEditionGroup},
+  repositories::edition_group_repository::create_edition_group,
+};
 
 #[utoipa::path(
     post,
@@ -18,7 +20,7 @@ pub async fn add_edition_group(
     arc: web::Data<Arcadia>,
     current_user_id: UserId,
 ) -> Result<HttpResponse> {
-    let edition_group = create_edition_group(&arc.pool, &form, current_user_id.0).await?;
+    let edition_group = create_edition_group(arc.pool.borrow(), &form, current_user_id.0).await?;
 
     Ok(HttpResponse::Created().json(edition_group))
 }
