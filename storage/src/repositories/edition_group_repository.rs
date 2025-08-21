d65dd9a9f581e489d69ae1334ae6cafd25d2b601
ppoliani <ppoliani@gmail.com>
2025-08-21 16:48:34 +0300

    feat: impl connection pool for invitation and gift

--- storage/src/repositories/edition_group_repository.rs
+++ storage/src/repositories/edition_group_repository.rs
@@ -1,34 +1,35 @@
-use sqlx::PgPool;
 use arcadia_common::error::{Error, Result};
 use crate::{
-    models::edition_group::{EditionGroup, UserCreatedEditionGroup},
+    connection_pool::ConnectionPool, models::edition_group::{EditionGroup, UserCreatedEditionGroup}
 };
 
-pub async fn create_edition_group(
-    pool: &PgPool,
-    edition_group_form: &UserCreatedEditionGroup,
-    current_user_id: i64,
-) -> Result<EditionGroup> {
-    const CREATE_EDITION_GROUPS_QUERY: &str = r#"
-        INSERT INTO edition_groups (title_group_id, name, release_date, created_by_id, description, distributor, covers, external_links, source, additional_information) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9::source_enum, $10)
-        RETURNING *;
-    "#;
+impl ConnectionPool {
+    pub async fn create_edition_group(
+        &self,
+        edition_group_form: &UserCreatedEditionGroup,
+        current_user_id: i64,
+    ) -> Result<EditionGroup> {
+        const CREATE_EDITION_GROUPS_QUERY: &str = r#"
+            INSERT INTO edition_groups (title_group_id, name, release_date, created_by_id, description, distributor, covers, external_links, source, additional_information) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9::source_enum, $10)
+            RETURNING *;
+        "#;
 
-    let created_edition_group = sqlx::query_as::<_, EditionGroup>(CREATE_EDITION_GROUPS_QUERY)
-        .bind(edition_group_form.title_group_id)
-        .bind(&edition_group_form.name)
-        .bind(edition_group_form.release_date)
-        .bind(current_user_id)
-        .bind(&edition_group_form.description)
-        .bind(&edition_group_form.distributor)
-        .bind(&edition_group_form.covers)
-        .bind(&edition_group_form.external_links)
-        .bind(&edition_group_form.source)
-        .bind(&edition_group_form.additional_information)
-        .fetch_one(pool)
-        .await
-        .map_err(Error::CouldNotCreateEditionGroup)?;
+        let created_edition_group = sqlx::query_as::<_, EditionGroup>(CREATE_EDITION_GROUPS_QUERY)
+            .bind(edition_group_form.title_group_id)
+            .bind(&edition_group_form.name)
+            .bind(edition_group_form.release_date)
+            .bind(current_user_id)
+            .bind(&edition_group_form.description)
+            .bind(&edition_group_form.distributor)
+            .bind(&edition_group_form.covers)
+            .bind(&edition_group_form.external_links)
+            .bind(&edition_group_form.source)
+            .bind(&edition_group_form.additional_information)
+            .fetch_one(self.borrow())
+            .await
+            .map_err(Error::CouldNotCreateEditionGroup)?;
 
-    Ok(created_edition_group)
+        Ok(created_edition_group)
+    }
 }
