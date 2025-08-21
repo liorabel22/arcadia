b12cfbc42cc06b3289ab84a0dad3461b872d038d
ppoliani <ppoliani@gmail.com>
2025-08-21 16:55:53 +0300

    feat: impl connection pool for subs

--- storage/src/repositories/subscriptions_repository.rs
+++ storage/src/repositories/subscriptions_repository.rs
@@ -1,44 +1,46 @@
 use sqlx::PgPool;
 use arcadia_common::error::{Error, Result};
 
-pub async fn create_subscription(
-    pool: &PgPool,
-    item_id: i64,
-    item: &str, // TODO: should only be one of the existing columns of the table
-    current_user_id: i64,
-) -> Result<()> {
-    sqlx::query(&format!(
-        "
-            INSERT INTO subscriptions ({item}_id, subscriber_id)
-            VALUES ($1, $2)
-        "
-    ))
-    .bind(item_id)
-    .bind(current_user_id)
-    .execute(pool)
-    .await
-    .map_err(Error::CouldNotCreateSubscription)?;
+impl ConnectionPool {
+    pub async fn create_subscription(
+        &self,
+        item_id: i64,
+        item: &str, // TODO: should only be one of the existing columns of the table
+        current_user_id: i64,
+    ) -> Result<()> {
+        sqlx::query(&format!(
+            "
+                INSERT INTO subscriptions ({item}_id, subscriber_id)
+                VALUES ($1, $2)
+            "
+        ))
+        .bind(item_id)
+        .bind(current_user_id)
+        .execute(self.borrow())
+        .await
+        .map_err(Error::CouldNotCreateSubscription)?;
 
-    Ok(())
-}
+        Ok(())
+    }
 
-pub async fn delete_subscription(
-    pool: &PgPool,
-    item_id: i64,
-    item: &str,
-    current_user_id: i64,
-) -> Result<()> {
-    let _ = sqlx::query(&format!(
-        "
-            DELETE FROM subscriptions
-            WHERE {item}_id = $1 AND subscriber_id = $2;
-        "
-    ))
-    .bind(item_id)
-    .bind(current_user_id)
-    .execute(pool)
-    .await?;
+    pub async fn delete_subscription(
+        &self,
+        item_id: i64,
+        item: &str,
+        current_user_id: i64,
+    ) -> Result<()> {
+        let _ = sqlx::query(&format!(
+            "
+                DELETE FROM subscriptions
+                WHERE {item}_id = $1 AND subscriber_id = $2;
+            "
+        ))
+        .bind(item_id)
+        .bind(current_user_id)
+        .execute(self.borrow())
+        .await?;
 
-    // TODO: check result.rows_affected()
-    Ok(())
+        // TODO: check result.rows_affected()
+        Ok(())
+    }
 }
