a88c2484f6bc15b0dc972d8158c11b2366314c9f
ppoliani <ppoliani@gmail.com>
2025-08-21 16:49:15 +0300

    feat: impl connection pool for master group

--- storage/src/repositories/master_group_repository.rs
+++ storage/src/repositories/master_group_repository.rs
@@ -1,27 +1,28 @@
-use sqlx::PgPool;
 use arcadia_common::error::{Error, Result};
 use crate::{
-    models::master_group::{MasterGroup, UserCreatedMasterGroup},
+    connection_pool::ConnectionPool, models::master_group::{MasterGroup, UserCreatedMasterGroup}
 };
 
-pub async fn create_master_group(
-    pool: &PgPool,
-    master_group_form: &UserCreatedMasterGroup,
-    current_user_id: i64,
-) -> Result<MasterGroup> {
-    let created_master_group = sqlx::query_as!(
-        MasterGroup,
-        r#"
-            INSERT INTO master_groups (name,created_by_id)
-            VALUES ($1, $2)
-            RETURNING *
-        "#,
-        master_group_form.name,
-        current_user_id
-    )
-    .fetch_one(pool)
-    .await
-    .map_err(Error::CouldNotCreateMasterGroup)?;
+impl ConnectionPool {
+    pub async fn create_master_group(
+        &self,
+        master_group_form: &UserCreatedMasterGroup,
+        current_user_id: i64,
+    ) -> Result<MasterGroup> {
+        let created_master_group = sqlx::query_as!(
+            MasterGroup,
+            r#"
+                INSERT INTO master_groups (name,created_by_id)
+                VALUES ($1, $2)
+                RETURNING *
+            "#,
+            master_group_form.name,
+            current_user_id
+        )
+        .fetch_one(self.borrow())
+        .await
+        .map_err(Error::CouldNotCreateMasterGroup)?;
 
-    Ok(created_master_group)
+        Ok(created_master_group)
+    }
 }
