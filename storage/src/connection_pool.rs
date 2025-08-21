519d1e228b02e0dab4c33ab0d0f4a22cdc70ceaf
ppoliani <ppoliani@gmail.com>
2025-08-21 13:30:53 +0300

    fix: use the correct imports in torrent_handler

--- storage/src/connection_pool.rs
+++ storage/src/connection_pool.rs
@@ -1,10 +1,15 @@
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 pub struct ConnectionPool(PgPool);
 
 impl ConnectionPool {
   pub async fn try_new(db_uri: &str) -> Result<Self, sqlx::Error> {
-    let pool = PgPool::connect(db_uri).await?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_uri)
+        .await
+        .expect("Error building a connection pool");
+
     Ok(Self(pool))
   }
 
