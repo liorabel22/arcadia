597ce26acf4b55bcd363e7f521c90581df03233d
ppoliani <ppoliani@gmail.com>
2025-08-28 17:31:42 +0300

    chore: add auth guard to one search endpoint

--- backend/api/src/handlers/search/mod.rs
+++ backend/api/src/handlers/search/mod.rs
@@ -4,13 +4,21 @@ pub mod search_title_group_info_lite;
 pub mod search_torrent_requests;
 pub mod search_torrents;
 
+use crate::middlewares::jwt_middleware::authenticate_user;
 use actix_web::web::{post, resource, ServiceConfig};
+use actix_web_httpauth::middleware::HttpAuthentication;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(
         resource("/title-groups/lite").route(post().to(self::search_title_group_info_lite::exec)),
     );
-    cfg.service(resource("/torrents/lite").route(post().to(self::search_torrents::exec)));
+    cfg.service(
+        resource("/torrents/lite").route(
+            post()
+                .to(self::search_torrents::exec)
+                .wrap(HttpAuthentication::with_fn(authenticate_user)),
+        ),
+    );
     cfg.service(resource("/artists/lite").route(post().to(self::search_artists_lite::exec)));
     cfg.service(
         resource("/torrent-requests").route(post().to(self::search_torrent_requests::exec)),
