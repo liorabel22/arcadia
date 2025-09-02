fb4ec563109ca220c0c12f2d1950769ef8a541ed
ppoliani <ppoliani@gmail.com>
2025-09-02 18:04:11 +0300

    chore: rebase on main

--- backend/api/tests/test_auth.rs
+++ backend/api/tests/test_auth.rs
@@ -7,9 +7,7 @@ use actix_web::{
 };
 use arcadia_api::{services::auth::InvalidationEntry, OpenSignups};
 use arcadia_storage::{
-    connection_pool::ConnectionPool,
-    models::user::{RefreshToken, UserClass},
-    redis::RedisInterface,
+    connection_pool::ConnectionPool, models::user::RefreshToken, redis::RedisInterface,
 };
 use mocks::mock_redis::MockRedisPool;
 use serde::{Deserialize, Serialize};
